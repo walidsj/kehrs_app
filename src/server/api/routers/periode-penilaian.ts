f55a424822ef20d5ce58f18e3fbad9f22f041cfd
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2025-02-14 08:09:00 +0800

    refactor

--- src/server/api/routers/periode-penilaian.ts
+++ src/server/api/routers/periode-penilaian.ts
@@ -1,25 +1,11 @@
 import { createTRPCRouter } from '@/server/api/trpc'
 import { authProcedure } from '../procedures'
-import { and, eq } from 'drizzle-orm'
+import { eq } from 'drizzle-orm'
 import { z } from 'zod'
 
 export const periodePenilaianRouter = createTRPCRouter({
   getAllPeriodePenilaian: authProcedure.query(async ({ ctx }) => {
-    const penilaian = await ctx.db.query.periodePenilaian.findMany()
-
-    return penilaian.map(async (p) => {
-      const isPenilaianExist = await ctx.db.query.penilaian.findFirst({
-        where: and(
-          eq(ctx.tables.penilaian.periodePenilaianId, p.id),
-          eq(ctx.tables.penilaian.unitId, ctx.user.unitId!),
-        ),
-      })
-
-      return {
-        ...p,
-        isPenilaianExist: !!isPenilaianExist,
-      }
-    })
+    return await ctx.db.query.periodePenilaian.findMany()
   }),
 
   getPeriodePenilaianById: authProcedure.input(z.object({ id: z.number() })).query(async ({ ctx, input }) => {
